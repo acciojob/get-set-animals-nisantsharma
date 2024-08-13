a50e7b7fbd4bba6d497795264f4c99eb578b7a56
NISANT SHARMA <106690246+nisantsharma@users.noreply.github.com>
2024-08-13 20:10:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
-//complete this code
-class Animal {}
+// Animal class definition
+class Animal {
+  constructor(species) {
+    this._species = species;
+  }
 
-class Dog extends Animal {}
+  // Getter for species
+  get species() {
+    return this._species;
+  }
 
-class Cat extends Animal {}
+  // Method to log the sound an animal makes
+  makeSound() {
+    console.log(`The ${this.species} makes a sound`);
+  }
+}
+
+// Dog class definition, extending Animal
+class Dog extends Animal {
+  // Method to log the sound a dog makes
+  bark() {
+    console.log("woof");
+  }
+}
+
+// Cat class definition, extending Animal
+class Cat extends Animal {
+  // Method to log the sound a cat makes
+  purr() {
+    console.log("purr");
+  }
+}
 
 // Do not change the code below this line
 window.Animal = Animal;
